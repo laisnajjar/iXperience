55a02ab5f317b06598e212ea6aca7f1666282b71
Lais Najjar <laisnajj@umich.edu>
2022-07-18 13:19:08 -0500

    commit

---
+++ Day6/task-list/src/components/TaskTable.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function TaskTable() {
+  return (
+    <div>
+        <div>
+        <div className="input-group mb-3">
+        <input type="text" class="form-control" placeholder="eneter task here" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+        <button className="input-group-text" id="basic-addon2">+</button>
+        </div>
+    </div>
+    </div>
+  )
+}
