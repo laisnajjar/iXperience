2876543d15029d72e2fa5d6bbf272b467af7c795
Lais Najjar <laisnajj@umich.edu>
2022-07-21 11:06:43 -0500

    correcting buttons

--- Day6/task-list/src/components/TaskTable.js
+++ Day6/task-list/src/components/TaskTable.js
@@ -1,57 +1,60 @@
 import React from 'react'
 
 export default function TaskTable(props) {
-  function toggleTaskComplete(task) {
-    task.complete = !task.complete
-    props.onTaskUpdated(task)
+
+  function onToggleTaskClicked(task) {
+    task.complete = !task.complete;
+    props.onTaskUpdated(task);
   }
 
   function onTaskRemove(task) {
-    props.onTaskRemove(task)
+    props.onTaskRemove(task);
   }
 
   return (
-    <div>
-      <table class="table">
+    <div className='mt-3'>
+
+      <table className="table">
         <thead>
           <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Complete</th>
-            <th scope="col">Actions</th>
+            <th>Name</th>
+            <th>Complete</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          {props.tasks.map((task) => (
-            <tr key={task.id}>
-              <td>{task.name}</td>
-              <td>
-                {task.complete ? 
-                  <i classame="bi bi-circle-fill green"></i>
-                 : 
-                  <i classame="bi bi-circle green"></i>
-                }
-              </td>
-              <td>
-                <button
-                  onClick={(e) => toggleTaskComplete(task)}
-                  className="btn btn-primary btn-sm ms-2"
-                  // {task.complete ?
-                  //   <i className="bi bi-toggle-off"></i>
-                  //   :
-                  //   <i className="bi bi-toggle-on"></i>
-                  // }
-                ></button>
-                <button
-                  onClick={(e) => onTaskRemove(task)}
-                  className="btn btn-primary btn-sm ms-2"
-                >
-                  <i className="bi bi-trash"></i>
-                </button>
-              </td>
-            </tr>
-          ))}
+
+          {
+            props.tasks.map((task) =>
+              <tr key={task.id}>
+                <td>{task.name}</td>
+                <td>
+                  <i className={
+                    task.complete ? "bi bi-circle-fill" : "bi bi-circle"
+                  }></i>
+                </td>
+                <td>
+                  <button
+                    onClick={(e) => onToggleTaskClicked(task)}
+                    className='btn btn-primary btn-sm'>
+                    <i className={
+                    task.complete ? "bi bi-toggle-off" : "bi bi-toggle-on"
+                  }></i>
+                  </button>
+
+                  <button
+                    onClick={(e) => onTaskRemove(task)}
+                    className='btn btn-primary btn-sm ms-3'>
+                    <i className="bi bi-trash"></i>
+                  </button>
+                </td>
+              </tr>
+            )
+          }
+
         </tbody>
       </table>
+
     </div>
   )
 }
