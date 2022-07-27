75cfe0a2879790f5240eef30217b31acee6c85d8
Lais Najjar <laisnajj@umich.edu>
2022-07-27 16:52:39 -0500

    added spinners

---
+++ Day9/recipe-app/src/components/tasks/Button.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Spinner from './Spinner';
+
+
+export default function Button({
+  variant = 'primary',
+  className = '',
+  onClick,
+  loading = false,
+  disabled = false,
+  type,
+  children
+}) {
+  return (
+    <button
+      className={'button btn btn-' + variant + ' ' + className}
+      onClick={onClick}
+      disabled={disabled || loading}
+      type={type}
+    >
+      {children}
+
+      {
+        loading ?
+          <Spinner className='button-spinner' variant='light' />
+          :
+          <></>
+      }
+    </button>
+  )
+}
--- Day9/recipe-app/src/components/tasks/Input.js
+++ Day9/recipe-app/src/components/tasks/Input.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react'
 import { Dish } from '../../models/dish'
+import Spinner from './Spinner'
 
 export default function Input(props) {
   const [dishName, setDishName] = useState('')
   const [procedureName, setProcedureName] = useState('')
-  function onFormSubmitted(event) {
+  const [saving, setSaving] = useState(false)
+  async function onFormSubmitted(event) {
     event.preventDefault()
-    const dish = new Dish(
-        dishName,
-        procedureName,
-        null,
-    )
-    props.onDishCreated(dish)
-    setDishName('')
-    setProcedureName('')
+    const dish = new Dish(dishName, procedureName, null)
+    setSaving(true)
+    try {
+      await props.onDishCreated(dish)
+      setDishName('')
+      setProcedureName('')
+    } catch (err) {
+      console.log(err)
+    }
+    setSaving(false)
   }
 
   return (
@@ -43,8 +47,11 @@ export default function Input(props) {
             rows="3"
           ></textarea>
         </div>
-        <button class="btn btn-primary" type="Submit">
-          Save
+        <button
+          className="btn btn-primary"
+          type="submit"
+        >
+          {saving ? <Spinner variant="info" /> : 'save'}
         </button>
       </form>
     </div>
